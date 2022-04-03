63b47c5434e7825b097b1d2931ee02e261ffe131
svshriyansh <prince.vishwakarma2000@gmail.com>
2022-04-03 14:20:16 +0530

    initial commit

---
+++ Part A/test_connection.py
@@ -0,0 +1,77 @@
+from pymongo import MongoClient
+import certifi
+import requests
+import sys
+from time import sleep
+# from  progress_bar import display_progress_bar,printProgressBar
+
+def get_database():
+
+    # Provide the mongodb atlas url to connect python to mongodb using pymongo
+    CONNECTION_STRING = "mongodb+srv://Shriyansh:eb0Q3dK7SPn0iSqM@cluster0.gpzb5.mongodb.net/tailnode?retryWrites=true&w=majority"
+    client = MongoClient(CONNECTION_STRING, tlsCAFile = certifi.where())
+    return client['tailnode']
+    
+# This is added so that many files can reuse the function get_database()
+def store_userData(collection_name):
+    url = 'https://dummyapi.io/data/v1/user'
+    headers = {
+    'app-id': '6246c4dc735be552983eed59'
+    }
+    r = requests.get(url, headers=headers)
+    file = r.json()
+    data = file['data']
+    print(f'Fetched records from {url}\ncount : {len(data)}')
+    collection_name.insert_many(data)
+
+def delete_collecion(collection_name):
+    print(f'data cleanup in process...\n')
+    collection_name.drop({})
+
+def get_users(collection_name, dbname):
+    cursor = collection_name.find({})
+    mydb = dbname['user_posts']
+    delete_collecion(mydb)
+    list_cursor = list(cursor)
+    max_bars_count = 20
+    spaces = f"%-{max_bars_count}s"
+    count = len(list_cursor)
+    print("Uploading\n")
+    i = 1
+    for document in list_cursor:
+
+        j = i / count
+        sys.stdout.write('\r')
+        sys.stdout.write(f"{spaces} %d%%" % ('#'*int(j*max_bars_count), 100*j))
+        sys.stdout.flush()
+        sleep(.25)
+
+        user_id = document['id']
+        posts = f'https://dummyapi.io/data/v1/user/{user_id}/post'
+        headers = {
+        'app-id': '6246c4dc735be552983eed59'
+        }
+        r = requests.get(posts, headers=headers)
+        file = r.json()
+        post_data = file['data']
+        mydb.insert_many(post_data)
+        i += 1
+    j = i / count
+    print("\n\nUploaded")
+
+    
+
+
+
+if __name__ == "__main__":    
+    
+    # Get the database
+    dbname = get_database()
+    collection_name = dbname['users']
+    # delete old records
+    delete_collecion(collection_name)
+    # store new records from API
+    store_userData(collection_name)
+    get_users(collection_name, dbname)
+
+
---
+++ Part B/script.py
@@ -0,0 +1,71 @@
+from bs4 import BeautifulSoup
+import requests
+from pymongo import MongoClient
+import certifi
+import sys
+
+def get_database():
+
+    CONNECTION_STRING = "mongodb+srv://Shriyansh:eb0Q3dK7SPn0iSqM@cluster0.gpzb5.mongodb.net/tailnode?retryWrites=true&w=majority"
+    client = MongoClient(CONNECTION_STRING, tlsCAFile = certifi.where())
+    print("Database Connected")
+    return client['tailnode']
+    
+def delete_collecion(collection_name):
+    print(f'dropping collection...')
+    collection_name.drop({})
+
+def store_userData(data,table):
+    table.insert_many(data)
+
+def book_data(soup):
+    obj = []
+    ol = soup.find('ol',{"class":"row"})
+    i = 1
+    list_ol = ol.find_all("li")
+    for li in list_ol:
+        arr = {}
+        h3 = li.find("h3")
+        a = h3.find("a")
+        book_name = a["title"]
+        price = li.find("p",{"class":"price_color"})
+        price = (price.text)
+        texts = li.find("p",{"class":"instock availability"}) 
+        stock = (texts.text.strip())
+        rateClass = li.find("p")
+        rate = rateClass['class'][1]
+        # print(rate)
+        arr['Name'] = book_name
+        arr['Price'] = price
+        arr['Avalibality'] = stock
+        arr['rating'] = rate
+        obj.append(arr)
+        i += 1
+    return obj
+
+def scrap(collection):
+    start = 1
+    last_page = 51
+    table = collection['data']
+    delete_collecion(table)
+    max_bars_count = 20
+    spaces = f"%-{max_bars_count}s"
+    for i in range(start,last_page):
+        count = last_page- start
+        j = i / count
+        sys.stdout.write('\r')
+        sys.stdout.write(f"{spaces} %d%%" % ('#'*int(j*max_bars_count), 100*j))
+        sys.stdout.flush()
+        no = i
+        url = f'http://books.toscrape.com/catalogue/page-{no}.html'
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text,"html5lib")
+        data = book_data(soup)
+        store_userData(data,table)
+    print("\nProcess Completed")
+
+
+if __name__ == "__main__":
+    collection = get_database()
+    scrap(collection)    
+    
